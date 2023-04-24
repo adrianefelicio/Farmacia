4988f31cba74e1283c8d8891ab66185abf4892a3
Adriane Felicio <adrianefelicios@outlook.com>
2023-04-24 11:39:53 -0300

    farmacia

---
+++ farmaciagen/src/main/java/com/farmacia/FarmaciagenApplication.java
@@ -0,0 +1,15 @@
+package com.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+
+public class FarmaciagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciagenApplication.class, args);
+	}
+
+}
